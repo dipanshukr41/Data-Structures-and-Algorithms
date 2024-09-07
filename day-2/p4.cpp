3ab3f83db53b06758921c6abcb828b73b8c9549e
Dipanshu Kumar <159459003+dipanshukr41@users.noreply.github.com>
2024-09-07 22:26:45 +0530

    Add files via upload

---
+++ day-2/p4.cpp
@@ -0,0 +1,16 @@
+// 123
+// 123
+// 123
+// 123
+#include<iostream>
+using namespace std;
+int main(){
+    int n=3;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<j+1;
+        }
+    cout<<endl;
+    }
+}
